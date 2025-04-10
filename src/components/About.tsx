fbe590eb1aa6b54e69bf7b0d10b13759406f3aff
wovvmac169 <wovvmac169@wovvmac169s-MacBook-Pro.local>
2025-04-10 12:27:58 +0530

    about

---
+++ src/components/About.tsx
@@ -0,0 +1,20 @@
+
+
+import '../App.css'
+
+
+function About() {
+
+
+  return (
+    <>
+    
+     <div className='container'>
+      <h1>Hi,I'm Anjali</h1>
+    
+      </div>
+    </>
+  )
+}
+
+export default About
